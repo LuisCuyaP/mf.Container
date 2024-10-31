b2f5e280cf29220ceffc921f14c34351be7b04c5
DevEnrqiue <luiscuyap@gmail.com>
2024-10-30 23:33:35 -0500

    Se modifica archivos para aceptar archivos .js d mf.Style

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-1. en  C:\Repositorios\Arquitectura\My.Org.Container\My.Org.Container
+1. en C:\Repositorios\Arquitectura\My.Org.Container\My.Org.Container
 2. ejecutar: create-single-spa
-npm install @angular/localize@17.3.12
-npm install @babel/core @babel/preset-env --save-dev
-npm install --save-dev babel-loader @babel/core @babel/preset-env
+   npm install @angular/localize@17.3.12
+   npm install @babel/core @babel/preset-env --save-dev
+   npm install --save-dev babel-loader @babel/core @babel/preset-env
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
 module.exports = (webpackConfigEnv, argv) => {
-	const orgName = "myorg";
-	const defaultConfig = singleSpaDefaults({
-		orgName,
-		projectName: "root-config",
-		webpackConfigEnv,
-		argv,
-		disableHtmlGeneration: true,
-	});
-	return merge(defaultConfig, {
-		// modify the webpack config however you'd like to by adding to this object
-		plugins: [
-			new HtmlWebpackPlugin({
-				inject: false,
-				template: "src/index.ejs",
-				templateParameters: {
-					isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
-					orgName,
-				},
-			}),
-		],
-	});
+  const orgName = "myorg";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: path.resolve(__dirname, "src/assets"), to: "assets" },
+        ],
+      }),
+    ],
+  });
 };
